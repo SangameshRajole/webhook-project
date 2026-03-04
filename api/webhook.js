6415bc8bf0181ca457eeb42c14fc35c0da1bf97e
SangameshRajole <sangameshrajole12@gmail.com>
2026-03-04 16:16:15 +0530

    fixes

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 // If the file is api/webhook.js, the endpoint is already /api/webhook
-app.post('/api/webhook', (req, res) => {
+app.post('/', (req, res) => {
   console.log('Received:', req.body);
   res.status(200).json({ 
     success: true, 
